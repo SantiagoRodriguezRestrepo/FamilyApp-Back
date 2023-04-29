ac31ba39fea7146a05200d050dd80e8cf273e809
lmljohanlml <cit.stiven@gmail.com>
2023-04-28 23:22:00 -0500

    ajuste en cors

--- src/main/java/proyecto/ean/demo/DemoApplication.java
+++ src/main/java/proyecto/ean/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package proyecto.ean.demo;
 import io.jsonwebtoken.JwtBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import proyecto.ean.demo.modelo.Usuario;
 import proyecto.ean.demo.servicios.ServicioObtenerToken;
 
@@ -13,7 +16,16 @@ public class DemoApplication {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 		SpringApplication.run(DemoApplication.class, args);
-
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigure(){
+		String[] urlConsumidoresApiPermitidosSeparados = "http://localhost:3000".split(";");
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("familiapp/**").allowedOrigins(urlConsumidoresApiPermitidosSeparados).allowedMethods("GET", "POST", "DELETE");
+			}
+		};
+	}
 }
